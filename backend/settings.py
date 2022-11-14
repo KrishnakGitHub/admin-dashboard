5604005995c9093d1940c3bc0eda499b13b1d5a3
krish <krishnakjee2016@gmail.com>
2022-11-14 18:04:35 +0000

    static

--- backend/settings.py
+++ backend/settings.py
@@ -168,12 +168,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'frontend/build/static'
+    'https://dr-admin-dashboard.herokuapp.com/static'
+
 ]
 
 
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect, useContext} from 'react';
 import AuthContext from '../context/AuthContext';
 
-// const domain = 'https://dr-admin-dashboard.herokuapp.com'
-const domain = 'https://8000-autumn-night-66818328.eu-ws4.runcode.io';
+const domain = 'https://dr-admin-dashboard.herokuapp.com'
+// const domain = 'https://8000-autumn-night-66818328.eu-ws4.runcode.io';
 
 const HomePage = () => {
     let [notes, setNotes] = useState([])
