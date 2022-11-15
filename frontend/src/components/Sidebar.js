a56f2d7f8b68f8d9ede626e3e18ddb4a4ac69f91
krish <krishnakjee2016@gmail.com>
2022-11-15 06:22:53 +0000

    added

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
+import ProjectPage from '../pages/ProjectPage';
 import {
   Nav,
   NavItem,
@@ -20,47 +22,41 @@ function Sidebar(props) {
   };
 
   return (
+    <Router>
     <Nav vertical>
       <Accordion open={open} toggle={toggle}>
         <AccordionItem>
           <AccordionHeader targetId="1"><i class="bi bi-house-door-fill"></i> Dashboard</AccordionHeader>
           <AccordionBody accordionId="1">
           <NavItem>
-            <NavLink href="/">
-              Dashboard
-            </NavLink>
+            <Link to="/">Dashboard</Link>
           </NavItem>
           </AccordionBody>
         </AccordionItem>
         <AccordionItem>
           <AccordionHeader targetId="2"><i class="bi bi-lightning-fill"></i> Project</AccordionHeader>
           <AccordionBody accordionId="2">
-          <NavLink href="/project">
-              Project
-            </NavLink>
-            <NavLink href="/project-details">
-              Project Details
-            </NavLink>
-            <NavLink href="/new-project">
-              New Project
-            </NavLink>
+          <NavItem>
+            <Link to="/project">Project</Link>
+          </NavItem>
+          <NavItem><Link to="/project-details">Project Details</Link></NavItem>
+          <NavItem><Link to="/new-project">New Project</Link></NavItem>
           </AccordionBody>
         </AccordionItem>
         <AccordionItem>
           <AccordionHeader targetId="3"><i class="bi bi-person-fill"></i> Client</AccordionHeader>
           <AccordionBody accordionId="3">
           <NavItem>
-            <NavLink href="/mc">
-              Manager Client
-            </NavLink>
-            <NavLink href="/clnt">
-              Client Details
-            </NavLink>
+            <Link to="/mc">Manager Client</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/clnt">Client Details</Link>
           </NavItem>
           </AccordionBody>
         </AccordionItem>
       </Accordion>
     </Nav>
+    </Router>
   );
 }
 
